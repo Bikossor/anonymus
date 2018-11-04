3e880d51fdbf9bb113b454cfac414d7e36c51d91
Andre-PC\Andre <andre.lichtenthaeler@gmail.com>
2018-11-04 17:01:33 +0100

    Closes #12

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+// Type definitions for anonymus 1.2.0
+// Definitions by: Bikossor <https://bikossor.de>
+
+export as namespace anonymus;
+
+/*~ Module methods */
+export function create(amount: number): string | string[];
+
+/*~ Module properties */
+export const colors: string[];
+export const animals: string[];
+export const sizeColors: number;
+export const sizeAnimals: number;
+export const sizeMax: number;
+export const randomColor: string;
+export const randomAnimal: string;
