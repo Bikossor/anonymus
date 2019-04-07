d3c8afd00957f204c1181ea5815447f37ad3a35f
Bikossor <andre.lichtenthaeler@gmail.com>
2019-04-07 21:09:26 +0200

    Closes #28

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,11 +1,18 @@
 (function() {
     var btnGenerate = document.getElementById("a_generate");
+    var btnCopy = document.getElementById("a_copy");
     var lblOutput = document.getElementById("a_result");
 
     function generateNewName() {
-        lblOutput.innerText = anonymus.create();
+        lblOutput.value = anonymus.create();
+    };
+
+    function copyGeneratedName() {
+        lblOutput.select();
+        document.execCommand("copy");
     };
 
     btnGenerate.addEventListener("click", generateNewName);
+    btnCopy.addEventListener("click", copyGeneratedName);
     window.addEventListener('load', generateNewName, false )
 })();
