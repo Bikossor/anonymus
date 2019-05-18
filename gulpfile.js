f7011b3c6dd8d6f1ba22a8cf22a9b8fdbfa6909e
Bikossor <andre.lichtenthaeler@gmail.com>
2019-05-18 22:49:50 +0200

    Closes #35

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,28 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var header = require('gulp-header');
 var pump = require('pump');
 
 var fileName = "anonymus";
 var folderDestination = "dist/";
 var folderDocs = "docs/assets/js/";
+var package = require('./package.json');
+var headerString = ['/*',
+	'<%= pkg.name %> v<%= pkg.version %> (<%= pkg.license %>) |',
+	'<%= pkg.author %> |',
+	'<%= pkg.homepage %>',
+	'*/\n'
+].join(' ');
 
 gulp.task("build-js", function (callback) {
 	pump([
 		gulp.src("index.js"),
 		uglify(),
 		concat(fileName + ".min.js"),
+		header(headerString, {
+			pkg: package
+		}),
 		gulp.dest(folderDestination),
 		gulp.dest(folderDocs)
 	], callback);
