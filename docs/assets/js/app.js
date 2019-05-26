1b2faff03c156d3e5cd255e300fadd2f1f203b44
Bikossor <andre.lichtenthaeler@gmail.com>
2019-05-26 21:03:13 +0200

    Closes #41

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,12 +1,13 @@
-(function() {
+(function () {
     var btnGenerate = document.getElementById("a_generate");
     var btnCopy = document.getElementById("a_copy");
     var lblOutput = document.getElementById("a_result");
+    var labelPackageVersion = document.getElementById("package_version");
 
     function generateNewName() {
         lblOutput.value = anonymus.create();
     };
-
+    
     function copyGeneratedName() {
         lblOutput.select();
         document.execCommand("copy");
@@ -14,5 +15,10 @@
 
     btnGenerate.addEventListener("click", generateNewName);
     btnCopy.addEventListener("click", copyGeneratedName);
-    window.addEventListener('load', generateNewName, false )
+
+    window.addEventListener('load', function () {
+        generateNewName();
+
+        labelPackageVersion.innerHTML = anonymus.version;
+    }, false);
 })();
