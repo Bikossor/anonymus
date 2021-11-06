8643f107838c9ede4535dadb6786ded57ac78b2a
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2021-11-06 03:56:02 +0100

    Update FeatureList component
    #104

--- packages/anonymus-website/src/components/HomepageFeatures.tsx
+++ packages/anonymus-website/src/components/HomepageFeatures.tsx
@@ -16,33 +16,20 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    image: '/img/undraw_docusaurus_mountain.svg',
+    title: "Easy to use",
+    image: "/img/undraw_code_thinking.svg",
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Anonymus was designed from the ground up to be easily installed and used
+        in your projects.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    image: '/img/undraw_docusaurus_tree.svg',
+    title: "Supports TypeScript",
+    image: "/img/undraw_proud_coder.svg",
     description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    image: '/img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
+      <>Anonymus ships with type declarations for TypeScript out-of-the-box.</>
     ),
   },
 ];
